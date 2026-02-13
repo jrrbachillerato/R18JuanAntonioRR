844b595fe45c0b4b03afc8b08b49c9c4f5c68266
Juan Antonio Ruiz Ramírez <jruiram477@g.educaand.es>
2026-02-13 10:06:49 +0100

    13-02-2026

--- R18E02JuanAntonioRR/Producto.cs
+++ R18E02JuanAntonioRR/Producto.cs
@@ -13,11 +13,32 @@ namespace R18E02JuanAntonioRR
         private const float PRECIO_MAX = 350;
         private const float IVA = 0.21F;
 
+        private const string NOMBRE_DEFAULT = "Desconocido";
+        private const float PRECIO_DEFAULT = 0;
+
         // CAMPOS | MIEMBROS
         private string _nombre;
         private float _precio;
 
         #region CONSTRUCTORES
+        /// <summary>
+        /// Instanciación del objeto Producto
+        /// </summary>
+        public Producto()
+        {
+            _nombre = NOMBRE_DEFAULT;
+            _precio = PRECIO_DEFAULT;
+        }
+        public Producto(string nombre)
+        {
+            Nombre = nombre;
+            _precio = PRECIO_DEFAULT;
+        }
+        public Producto(string nombre, float precio)
+        {
+            Nombre = nombre;
+            Precio = precio;
+        }
 
         #endregion
 
@@ -26,21 +47,31 @@ namespace R18E02JuanAntonioRR
         {
             get
             {
+                if (_nombre == NOMBRE_DEFAULT)
+                {
+                    throw new ArgumentException("El nombre del producto no ha sido inicializado");
+                }
                 return _nombre;
             }
             set
             {
+                ValidarNombre(value);
                 _nombre = value;
             }
         }
         public float Precio
         {
             get
-            {
+            { 
+                if(_precio == PRECIO_DEFAULT)
+                {
+                    throw new ArgumentException("El precio del producto no está establecido");
+                }
                 return _precio;
             }
             set
             {
+                ValidarPrecio(value);
                 _precio = value;
             }
         }
@@ -48,7 +79,7 @@ namespace R18E02JuanAntonioRR
         {
             get
             {
-                return 0;
+                return CalcularPrecioIva();
             }
         }
         #endregion
